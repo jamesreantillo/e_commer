74b1fd1b2c95f94156d7e6d8fcea2861feda375b
james reantillo <james.reantillo@gmail.com>
2021-03-19 00:23:13 -0700

    add router

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,60 @@
 import React from 'react';
-import {AppBar, Toolbar, IconButton, Badge, MenuItem,Menu, Typography} from '@material-ui/core';
-import {ShoppingCart} from '@material-ui/icons';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import { Link, useLocation } from 'react-router-dom';
 
-import useStyles from './styles'
+import useStyles from './styles';
 
-const Navbar = ({totalItems}) => {
+const Navbar = ({ totalItems }) => {
   const classes = useStyles();
-  return ( 
+  const location = useLocation();
+
+  return (
     <>
-     <AppBar position='fixed' className={classes.appBar} color='inherit'>
-      <Toolbar>
-        <Typography variant='h6' className={classes.title} color='inherit'>
-          <img src="https://via.placeholder.com/150" alt='commerce.js' height='25px' className={classes.image}/>
-          Commerce
-        </Typography>
-        <div className={classes.grow}></div>
-        <div className={classes.button}>
-          <IconButton aria-label="Show cart items" color='inherit'>
-            <Badge badgeContent={totalItems} color='secondary'>
-              <ShoppingCart/>
-            </Badge>
-          </IconButton>
-        </div>
-      </Toolbar>
-     </AppBar> 
+      <AppBar position='fixed' className={classes.appBar} color='inherit'>
+        <Toolbar>
+          <Typography
+            component={Link}
+            to='/'
+            variant='h6'
+            className={classes.title}
+            color='inherit'
+          >
+            <img
+              src='https://via.placeholder.com/150'
+              alt='commerce.js'
+              height='25px'
+              className={classes.image}
+            />
+            Commerce
+          </Typography>
+          <div className={classes.grow} />
+          {location.pathname === '/' && (
+            <div className={classes.button}>
+              <IconButton
+                component={Link}
+                to='/cart'
+                aria-label='Show cart items'
+                color='inherit'
+              >
+                <Badge badgeContent={totalItems} color='secondary'>
+                  <ShoppingCart />
+                </Badge>
+              </IconButton>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
     </>
-   );
-}
- 
+  );
+};
+
 export default Navbar;
