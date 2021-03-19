c3017049dd95ccd38db9e91a712d9c5716df110c
james reantillo <james.reantillo@gmail.com>
2021-03-19 00:45:40 -0700

    add deduct and remove item functionality

--- src/App.js
+++ src/App.js
@@ -20,10 +20,25 @@ const App = () => {
   };
 
   const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, 1);
-    console.log('item', item);
-    setCart(item.cart);
+    const {cart} = await commerce.cart.add(productId, 1);
+  
+    setCart(cart);
   };
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const {cart} = await commerce.cart.update(productId, { quantity });
+    setCart(cart)
+  };
+
+  const handleRemoveFromCart = async (productId) => {
+    const {cart} = await commerce.cart.remove(productId)
+    setCart(cart)
+  }
+
+  const handleEmptyCart = async () => {
+    const {cart} = await commerce.cart.empty()
+    setCart(cart)
+  }
   useEffect(() => {
     fetchProducts();
     fetchCart();
@@ -39,7 +54,7 @@ const App = () => {
             <Products products={products} onAddToCart={handleAddToCart} />
           </Route>
           <Route exact path='/cart'>
-            <Cart cart={cart} />
+            <Cart cart={cart} handleUpdateCartQty={handleUpdateCartQty} handleRemoveFromCart={handleRemoveFromCart} handleEmptyCart={handleEmptyCart}/>
           </Route>
         </Switch>
       </div>
